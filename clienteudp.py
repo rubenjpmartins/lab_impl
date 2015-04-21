c276fc552ab242bbf35c90e02ddd3b40c2384b18
Rúben Martins <rubenjpmartins@gmail.com>
2015-04-21 19:22:09 +0100

    Update clienteudp.py

--- clienteudp.py
+++ clienteudp.py
@@ -6,7 +6,7 @@
 # 2    Reconhecimento do registo (ACK);
 # 3    Mensagem (DATA);
 # 4    Reconhecimento de mensagem enviada (MSG ACK); 
-# 5    Reconhecimento de mensagem recebida no destinatario 
+# 5    Reconhecimento de mensagem recebida no destinatario
 # 6    Reconhecimento de mensagem nao enviada (ERRO); 
 # 7    Terminacao da ligacao (FIM);
 # 8    Indicacao ao outro user da saida de um deles
@@ -20,10 +20,10 @@ print "Modo?"
 
 Input="1 "+raw_input()
 
-print "Nome?"
+print "Quem es?"
 Input = Input + " "+raw_input()
 
-print "Nome do destinatario?"
+print "Para quem?"
 Input=	Input +" "+raw_input()
 
 flag=0
@@ -35,11 +35,11 @@ ClientSock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 # Envia o registo do user para o servidor
 ClientSock.sendto(Input,('localhost',	5005))
-print	"Mensagem para o servidor: " +	Input
+print	"Menssagem para o servidor: " +	Input
 
 # Recebe mensagem do servidor
 (ServerMsg,	(ServerIP,	ServerPort))	=	ClientSock.recvfrom (1024)  
-print	"Mensagem do servidor: 2 Estas registado!  "
+print	"Menssagem do servidor: 2 Estas registado!  "
 print "modo - "+ ServerMsg
 
 #modo (receiver/sender)
@@ -60,9 +60,9 @@ while True:
 			msg = "FIM"
 			msgtoserver="3"+" "+ msg
 			ClientSock.sendto(msgtoserver,('localhost',	5005))
-			print	("Mensagem para o servidor: " +	msg)
+			print	("Menssagem para o servidor: " +	msg)
 			(ServerMsg,	(ServerIP,	ServerPort))	=	ClientSock.recvfrom (1024)	
-			print	("Mensagem do servidor: " +	ServerMsg)
+			print	("Message from server: " +	ServerMsg)
 			print "Adeus"
 			ClientSock.close ()
 			break	
@@ -71,9 +71,9 @@ while True:
 		#Se o sender quiser sair
 		if msg == "FIM":
 			ClientSock.sendto(msgtoserver,('localhost',	5005))
-			print	("Mensagem para o servidor: " +	msg)
+			print	("Menssagem para o servidor: " +	msg)
 			(ServerMsg,	(ServerIP,	ServerPort))	=	ClientSock.recvfrom (1024)	
-			print	("Mensagem do servidor: " +	ServerMsg)
+			print	("Message from server: " +	ServerMsg)
 			print "Adeus"
 			ClientSock.close ()
 			break	
@@ -81,25 +81,25 @@ while True:
 		# Envia input para o servidor	
 		print "vou tentar enviar"
 		ClientSock.sendto(msgtoserver,('localhost',	5005))
-		print	("Mensagem do servidor: " + msgtoserver)
+		print	("Message to server: " + msgtoserver)
 
 		(ServerMsg,	(ServerIP,	ServerPort))	=	ClientSock.recvfrom (1024)	
-		print	("Mensagem do servidor: " +	ServerMsg)
+		print	("Message from server: " +	ServerMsg)
 
 		#Se o outro user n tiver registado
 		if ServerMsg!="6 Reconhecimento de mensagem nao enviada (ERRO)":	
-			modo="receiver"
+			modo="reciver"
 			print "modo - "+ modo
 
 	# se for o leitor	
-	if modo =="receiver":
+	if modo =="reciver":
 		(ServerMsg,	(ServerIP,	ServerPort))	=	ClientSock.recvfrom (1024)	
 		print	("Message from server: " +	ServerMsg)
 
 		if ServerMsg=="8 - o utilizador saiu":
 			flag = 1
 
-		msgtoserver="5 Mensagem recebida no destinatario"
+		msgtoserver="5 USER ACK"
 		ClientSock.sendto(msgtoserver,('localhost',	5005))
 		
 		modo="sender"
