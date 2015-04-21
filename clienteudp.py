8730a753ff3cf15d413665423434d551fd194937
Rúben Martins <rubenjpmartins@gmail.com>
2015-04-21 18:06:27 +0100

    Update clienteudp.py

--- clienteudp.py
+++ clienteudp.py
@@ -1,24 +1,116 @@
-#udp-client.py
+# Cliente
+
+# PTSM Opcodes
+#
+# 1    Pedido de registo (PR);
+# 2    Reconhecimento do registo (ACK);
+# 3    Mensagem (DATA);
+# 4    Reconhecimento de mensagem enviada (MSG ACK); 
+# 5    Reconhecimento de mensagem recebida no destinatario 
+# 6    Reconhecimento de mensagem nao enviada (ERRO); 
+# 7    Terminacao da ligacao (FIM);
+# 8    Indicacao ao outro user da saida de um deles
+
 
 import socket
+print "ICHAT CLIENTE"
+print
+print "Modo?"
 
-ClientSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-WriteRequest = '\x00'+'\x02' + 'abcd.txt'+'\x00'+'netascii'+'\x00'
-TFTP.sendto(WriteRequest,('192.168.56.101.'))
-ClientMsg = "O Toni é o máior"
-ClientSock.sendto(ClientMsg,('192.168.56.101',5005))
-print "Message to erver: ", ClientMsg
-(ServerMsg(ServerIP, ServerPort)) = ClientSock.recvfrom(100)
-print "Message from server: ", ServerMsg
-ClientSock.close()
 
-#correcta
+Input="1 "+raw_input()
 
-import socket
-ClientSock=socket.socket(socket.AF_INET,	socket.SOCK_DGRAM)	
-ClientSock.sendto(Registery,	('localhost',	7006))
-print	("Message	to	server:	",	Registery)
-(ServerMsg,	(ServerIP,	ServerPort))	=	ClientSock.recvfrom (1024)	
-print	("Message from server: ",	ServerMsg)
-ClientSock.close ()
+print "Nome?"
+Input = Input + " "+raw_input()
+
+print "Nome do destinatario?"
+Input=	Input +" "+raw_input()
+
+flag=0
+
+
+#Inicio do Socket
+
+ClientSock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+# Envia o registo do user para o servidor
+ClientSock.sendto(Input,('localhost',	5005))
+print	"Mensagem para o servidor: " +	Input
+
+# Recebe mensagem do servidor
+(ServerMsg,	(ServerIP,	ServerPort))	=	ClientSock.recvfrom (1024)  
+print	"Mensagem do servidor: 2 Estas registado!  "
+print "modo - "+ ServerMsg
+
+#modo (receiver/sender)
+modo=ServerMsg
+
+
+# Ciclo apos resgisto
+while True:
+	# se for o escritor 
+	if modo=="sender":
+		msg = raw_input("> ")
+		
+		#Opcode 3 - Mensagem
+		msgtoserver="3"+" "+ msg
+		
+		if flag==1:
+			print "Vou sair tambem"
+			msg = "FIM"
+			msgtoserver="3"+" "+ msg
+			ClientSock.sendto(msgtoserver,('localhost',	5005))
+			print	("Mensagem para o servidor: " +	msg)
+			(ServerMsg,	(ServerIP,	ServerPort))	=	ClientSock.recvfrom (1024)	
+			print	("Mensagem do servidor: " +	ServerMsg)
+			print "Adeus"
+			ClientSock.close ()
+			break	
+
+
+		#Se o sender quiser sair
+		if msg == "FIM":
+			ClientSock.sendto(msgtoserver,('localhost',	5005))
+			print	("Mensagem para o servidor: " +	msg)
+			(ServerMsg,	(ServerIP,	ServerPort))	=	ClientSock.recvfrom (1024)	
+			print	("Mensagem do servidor: " +	ServerMsg)
+			print "Adeus"
+			ClientSock.close ()
+			break	
+		
+		# Envia input para o servidor	
+		print "vou tentar enviar"
+		ClientSock.sendto(msgtoserver,('localhost',	5005))
+		print	("Mensagem do servidor: " + msgtoserver)
+
+		(ServerMsg,	(ServerIP,	ServerPort))	=	ClientSock.recvfrom (1024)	
+		print	("Mensagem do servidor: " +	ServerMsg)
+
+		#Se o outro user n tiver registado
+		if ServerMsg!="6 Reconhecimento de mensagem nao enviada (ERRO)":	
+			modo="receiver"
+			print "modo - "+ modo
+
+	# se for o leitor	
+	if modo =="receiver":
+		(ServerMsg,	(ServerIP,	ServerPort))	=	ClientSock.recvfrom (1024)	
+		print	("Message from server: " +	ServerMsg)
+
+		if ServerMsg=="8 - o utilizador saiu":
+			flag = 1
+
+		msgtoserver="5 Mensagem recebida no destinatario"
+		ClientSock.sendto(msgtoserver,('localhost',	5005))
+		
+		modo="sender"
+		print "modo - "+ modo
+
+
+
+
+
+
+
+
+
 
